d6fc40b8a51e37ba5bef59d748f1e9ed1ca5c30c
Ben Greaves <bsgreaves@gmail.com>
2017-07-25 14:28:10 -0500

    First draft written. Beginning testing

---
+++ Models/PaymentType.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonAPI.Models
+{
+    public class PaymentType
+    {
+        [Key]
+        public int PaymentTypeID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int AccountNumber {get; set; }
+        
+        [Required]
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
